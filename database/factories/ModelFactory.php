76cc8a3fe07b73c5a9ba31d8248570811360f76a
rahulalam31 <gaberialcramern31@outlook.com>
2024-05-24 23:06:18 +0530

    database and resources added

---
+++ database/factories/ModelFactory.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace rahulalam31\Larabot\Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/*
+class ModelFactory extends Factory
+{
+    protected $model = YourModel::class;
+
+    public function definition()
+    {
+        return [
+
+        ];
+    }
+}
+*/
