1c16164effdd727a84597fc245016168ac210b80
rahulalam31 <gaberialcramern31@outlook.com>
2024-05-26 21:59:08 +0530

    chatbot config update

--- config/chatbot.php
+++ config/chatbot.php
@@ -2,6 +2,16 @@
 
 
 return [
-    'api_key' => env('NLP_API_KEY', ''),
+    'default_service' => env('CHATBOT_DEFAULT_SERVICE', 'dialogflow'),
+    'dialogflow' => [
+        'api_key' => env('DIALOGFLOW_API_KEY', ''),
+    ],
+    'openai' => [
+        'api_key' => env('OPENAI_API_KEY', ''),
+        'model' => env('OPENAI_MODEL', 'text-davinci-003'),
+    ],
+    'llama' => [
+        'api_key' => env('LLAMA_API_KEY', ''),
+    ],
     'default_response' => 'I am not sure how to respond to that.',
 ];
